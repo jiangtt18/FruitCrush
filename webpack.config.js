f39ae712bc5d2f26602021f6044287fea5e198bc
jiangtt18 <tjiang90111@gmail.com>
2018-04-20 02:28:22 -0700

    add styleing

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
 	context: __dirname,
-	entry: './lib/ballcrush.js',
+	entry: './lib/bubblecrush.js',
 	output: {
 		path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
 		filename: 'bundle.js'
